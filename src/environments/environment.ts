635b20ea41baef704ef7bf4645b82c0d47cfdd38
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2026-02-18 20:00:09 -0300

    .

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,9 +2,9 @@ export const environment = {
     producion: false,
     apiUrl: "http://localhost:8000",
     auth0: {
-        domain: "",
-        clientId: "",
-        audience: "",
+        domain: "dev-q3fpumx4gqhhk3tm.us.auth0.com",
+        clientId: "saQvCdIsrq0EKMewY7q76OfHdKRaDufe",
+        audience: "019c72de-ba9a-7f55-a7b0-44299d046066",
         redirectUri: "http://localhost:4200/callback",
     }
 }
