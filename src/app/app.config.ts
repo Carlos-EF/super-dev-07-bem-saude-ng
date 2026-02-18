e14530e490d7fad504772f89e13b1288207af195
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2026-02-18 20:36:23 -0300

    .

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,12 +6,35 @@ import Aura from '@primeuix/themes/aura';
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { ConfirmationService, MessageService } from 'primeng/api';
+import {provideAuth0 } from '@auth0/auth0-angular';
+import { environment } from '../environments/environment';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideHttpClient(withFetch()),
+    provideAuth0({
+      domain: environment.auth0.domain,
+      clientId: environment.auth0.clientId,
+      authorizationParams: {
+        audience: environment.auth0.audience,
+        redirectUri: environment.auth0.redirectUri,
+      },
+      httpInterceptor: {
+        allowedList: [
+          {
+            uri: `${environment.apiUrl}/*`,
+            tokenOptions: {
+              authorizationParams: {
+                audience: environment.auth0.audience
+              }
+            }
+          }
+        ]
+      }
+    }),
     providePrimeNG({
       overlayAppendTo: "body",
       theme: {
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+    {
+        path: 'callback',
+        loadComponent: () => import('./pages/callback/callback').then((m) => m.Callback)
+    },
     {path: 'recepcionistas', loadComponent: () => import('./pages/recepcionistas/list/list').then((m) => m.List)},
     {path: 'pacientes', loadComponent: () => import('./pages/pacientes/list/list').then((m) => m.List)},
     {path: 'consultas', loadComponent: () => import('./pages/consultas/list/list').then((m) => m.List)},
