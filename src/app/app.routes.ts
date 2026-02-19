f76da0de75f0daecafb7617f299a8bcc982386f3
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2026-02-18 21:47:06 -0300

    Aula 2026-02-18.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,17 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {path: 'recepcionistas', loadComponent: () => import('./pages/recepcionistas/list/list').then((m) => m.List)},
-    {path: 'pacientes', loadComponent: () => import('./pages/pacientes/list/list').then((m) => m.List)},
-    {path: 'consultas', loadComponent: () => import('./pages/consultas/list/list').then((m) => m.List)},
-    {path: 'financeiro', loadComponent: () => import('./pages/financeiro/list/list').then((m) => m.List)},
+    {
+        path: 'callback',
+        loadComponent() => import("./pages/callback/callback").then((m) => m.Callback)
+    },
+    {
+        path: '',
+        loadComponent: () => import('./layout/layout').then((m) => m.Layout),
+            children: [
+                { path: 'recepcionistas', loadComponent: () => import('./pages/recepcionistas/list/list').then((m) => m.List) },
+                { path: 'pacientes', loadComponent: () => import('./pages/pacientes/list/list').then((m) => m.List) },
+                { path: 'profissionais', loadComponent: () => import('./pages/profissionais/list/list').then((m) => m.List) },
+            ]
+    },  
 ];
---
+++ src/app/pages/callback/callback.ts
@@ -0,0 +1,24 @@
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '@auth0/auth0-angular';
+import { filter } from 'rxjs';
+import { take } from 'rxjs/internal/operators/take';
+
+@Component({
+  selector: 'app-callback',
+  imports: [],
+  templateUrl: './callback.html',
+})
+export class Callback {
+  private readonly auth = inject(AuthService);
+  private readonly router = inject(Router);
+
+  ngOnInit() {
+    this.auth.isAuthenticated$.pipe(
+      filter((isAuthenticated) => isAuthenticated),
+      take(1)
+    ).subscribe(() => {
+      this.router.navigate(["/"]);
+    })
+  }
+}
